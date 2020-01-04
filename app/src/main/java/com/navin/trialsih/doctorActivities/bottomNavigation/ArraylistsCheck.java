4e5c33a4e552b72955286efaf7d03e7f639bb165
kundannitp <kundan3316@gmail.com>
2020-01-04 16:33:27 +0530

    KundanVoiceCommit

---
+++ app/src/main/java/com/navin/trialsih/doctorActivities/bottomNavigation/ArraylistsCheck.java
@@ -0,0 +1,73 @@
+package com.navin.trialsih.doctorActivities.bottomNavigation;
+
+import java.util.ArrayList;
+
+public class ArraylistsCheck {
+
+    ArrayList<String> preNames=new ArrayList<>();
+    ArrayList<String> preSymptoms=new ArrayList<>();
+    ArrayList<String> preDiagnose=new ArrayList<>();
+    ArrayList<String> prePrescription=new ArrayList<>();
+    ArrayList<String> preAdvice=new ArrayList<>();
+
+    public ArraylistsCheck(){
+        preNames.add("aarav");
+        preNames.add("aditya");
+        preNames.add("vihaan");
+        preNames.add("arjun");
+        preNames.add("reyansh");
+        preNames.add("muhammad");
+        preNames.add("sai");
+        preNames.add("ayaan");
+        preNames.add("aryan");
+        preNames.add("ansh");
+        preNames.add("rakesh");
+        preNames.add("navin");
+        preNames.add("naveen");
+        preNames.add("anant");
+        preNames.add("kundan");
+        preNames.add("prabhkirat");
+        preNames.add("manish");
+        preNames.add("harsh");
+        preNames.add("rohit");
+        preNames.add("ankit");
+        preNames.add("shaurya");
+        preNames.add("dhruv");
+        preNames.add("bharat");
+
+        preSymptoms.add("fever");
+        preSymptoms.add("headache");
+        preSymptoms.add("stomach pain");
+        preSymptoms.add("cough");
+        preSymptoms.add("loose motion");
+        preSymptoms.add("pain");
+        preSymptoms.add("pain in forearms");
+
+        preDiagnose.add("diabetes");
+        preDiagnose.add("multiple sclerosis");
+        preDiagnose.add("crohn and colitis");
+        preDiagnose.add("lupus");
+        preDiagnose.add("rheumatoid arthritis");
+        preDiagnose.add("allergy and asthma");
+        preDiagnose.add("celiac disease");
+        preDiagnose.add("scleroderma");
+        preDiagnose.add("infectious disease");
+
+        prePrescription.add("acetaminophen");
+        prePrescription.add("adderall");
+        prePrescription.add("alprazolam");
+        prePrescription.add("aitalopram");
+        prePrescription.add("clindamycin");
+        prePrescription.add("clonazepam");
+        prePrescription.add("codeine");
+        prePrescription.add("cyclobenzaprine");
+        prePrescription.add("lisinopril");
+        prePrescription.add("loratadine");
+        prePrescription.add("lorazepam");
+        prePrescription.add("omeprazole");
+        prePrescription.add("oxycodone");
+        prePrescription.add("pantoprazole");
+        prePrescription.add("paracetamol");
+    }
+
+}
