464a087aac181aa48d39d7890751fc6e9a6a827d
Prabhkiratnitp <prabhkiratntw99@gmail.com>
2020-01-05 18:05:00 +0530

    Java Mail Api

---
+++ app/src/main/java/com/navin/trialsih/doctorActivities/bottomNavigation/Utils.java
@@ -0,0 +1,9 @@
+package com.navin.trialsih.doctorActivities.bottomNavigation;
+
+public class Utils {
+
+    public static final String EMAIL = "prabhkiratntw99@gmail.com";
+
+    //This is your from email password
+    public static final String PASSWORD = "Avneet@123";
+}
